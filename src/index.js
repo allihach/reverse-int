1c5efefe5680a45cabaafcefe7ea2b8da8fa9910
allihach <lihach.ac@gmail.com>
2020-03-08 20:07:29 +0400

    feat: edit index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n) + '';
+    let arr = n.split('').reverse();
+    let str = '';
+
+    for(let i=0; i<arr.length; i++) {
+        str += arr[i];
+    }
+
+    return Number.parseInt(str);
 }
